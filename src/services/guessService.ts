e457924eff404250020d159b69661739c293b5b9
amitbar2001 <amitbar2001@gmail.com>
2022-08-06 15:21:28 +0300

    added semantleService

--- src/services/guessService.ts
+++ src/services/guessService.ts
@@ -1,12 +1,20 @@
-import { Prisma } from "@prisma/client";
 import * as repository from "../repositories/guessReopository";
+import guessWord from "./semantleService";
 
-export const addGuess = async (guess: Prisma.GuessUncheckedCreateInput) => ({
-  ...(await repository.addGuess(guess)),
-  serialNumber: await repository.getGuessesCountByTeam({
-    teamId: guess.teamId,
-  }),
-});
+export const addGuess = async (guess: {
+  word: string;
+  ownerId: string;
+  teamId: string;
+}) => {
+  const { similarity: score, distance: rank } = await guessWord(guess.word);
+
+  return {
+    ...(await repository.addGuess({ ...guess, score, rank })),
+    serialNumber: await repository.getGuessesCountByTeam({
+      teamId: guess.teamId,
+    }),
+  };
+};
 
 export const getAllGuesses = async () => repository.getAllGuesses();
 
